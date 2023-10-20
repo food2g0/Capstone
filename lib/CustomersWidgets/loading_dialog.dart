ee5aee9026458a66ca2d9f2a2da1d7b674665dc8
food2g0 <foodtogo763@gmail.com>
2023-10-20 23:25:09 +0800

    Initial commit

---
+++ lib/CustomersWidgets/loading_dialog.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:foodtogo_customer/CustomersWidgets/progress_bar.dart';
+
+
+
+class LoadingDialog extends StatelessWidget
+{
+  final String? message;
+  const LoadingDialog({super.key, this.message});
+
+  @override
+  Widget build(BuildContext context) {
+    return AlertDialog(
+      key: key,
+      content: Column(
+      mainAxisSize: MainAxisSize.min,
+        children: [
+          circularProgress(),
+          const SizedBox(height: 10,),
+          Text("${message!}, Please wait...."),
+
+        ],
+      ),
+
+    );
+  }
+}
---
+++ lib/assistantMethods/cart_item_counter.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/cupertino.dart';
+import 'package:foodtogo_customer/global/global.dart';
+
+class CartItemCounter extends ChangeNotifier
+{
+  int cartListItemCounter = sharedPreferences!.getStringList("userCart")!.length - 1;
+  int get count => cartListItemCounter;
+
+  Future<void> displayCartListItemNumber() async
+  {
+    cartListItemCounter = sharedPreferences!.getStringList("userCart")!.length - 1;
+    
+    await Future.delayed(const Duration(milliseconds: 100),() {
+      notifyListeners();
+    });
+  }
+}
